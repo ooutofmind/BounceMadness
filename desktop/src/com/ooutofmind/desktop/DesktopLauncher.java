0a1f21d4fb540b3306037b80abcf9a063915cb5e
outofmind <wofffk@gmail.com>
2020-04-24 23:32:25 +0500

    Initial commit

---
+++ desktop/src/com/ooutofmind/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ooutofmind.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ooutofmind.BounceMadness;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BounceMadness(), config);
+	}
+}
